cdc8db5ab405d04dad45ae8d86428104aa460011
Овсянников Владислав <vladikslav25@gmail.com>
2025-07-31 17:57:39 +0500

    new Button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
 import {useEffect} from "react";
+import {useTelegram} from "./components/hooks/useTelegram";
 
 
 function App() {
-
+    const {onToggleButton, tg} = useTelegram()
     useEffect(() => {
         tg.ready()
     }, [])
 
-
-
   return (
     <div className="App">
-      Work
+      <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
 }
